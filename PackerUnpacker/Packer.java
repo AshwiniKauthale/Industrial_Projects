f61b6fac7f5a62aa09326e8fcb6d8a84e0311717
AshwiniKauthale <kauthaleashwini40@gmail.com>
2026-02-01 22:18:29 +0530

    Add files via upload

--- PackerUnpacker/Packer.java
+++ PackerUnpacker/Packer.java
@@ -1,1 +1,151 @@
+import java.io.*;
+import java.util.*;
 
+class PackerX
+{
+    public String Header;
+
+    public byte Key;
+
+    public int iRet;
+    public int i;
+    public int j;
+
+    public byte Buffer[];
+    public byte bHeader[];
+
+    public File fArr[];
+
+    public File fobj;
+    File PackObj;
+    public FileInputStream fiobj;
+    public FileOutputStream foobj;
+
+    Scanner sobj;
+
+    public PackerX()
+    {
+        this.Header = null;
+
+        this.Key = 0x11;
+
+        this.iRet = 0;
+        this.i = 0;
+        this.j = 0;
+
+        this.Buffer = new byte[1024];
+        this.bHeader = new byte[100];
+
+        this.fArr = null;
+
+        this.fobj = null;
+        this.PackObj = null;
+        this.fiobj = null;
+        this.foobj = null;
+
+        this.sobj = null;
+    }
+
+    protected void Finalized()
+    {
+        try
+        {
+            fiobj.close();
+            foobj.close();
+        }
+        catch(IOException aobj)
+        {
+            System.out.println(aobj);
+        }
+        PackObj = null;
+        fobj = null;
+        sobj.close();
+    }
+
+    public void packing()
+    {
+        sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name of folder : ");
+        String FolderName = sobj.nextLine();
+
+        System.out.println("Enter the name of packed file : ");
+        String PackName = sobj.nextLine();
+
+        System.out.println("Enter the type of file :(.java,.c,.cpp,.txt)");
+        String FileExtension = sobj.nextLine();
+
+        fobj = new File(FolderName);
+
+        try
+        {
+            if((fobj.exists()) && (fobj.isDirectory()))
+            {
+                PackObj = new File(PackName);
+
+                PackObj.createNewFile();
+
+                foobj = new FileOutputStream(PackObj);
+                
+                System.out.println("Folder is present");
+
+                fArr = fobj.listFiles();
+                
+                System.out.println("Number of files in the folder are : "+fArr.length);
+
+                for(i = 0; i < fArr.length; i++)
+                {
+                    fiobj = new FileInputStream(fArr[i]);
+
+                    if(fArr[i].getName().endsWith(FileExtension))
+                    {
+                        // Header Formation
+                        Header = fArr[i].getName() + " " + fArr[i].length();
+                        for(j = Header.length(); j < 100; j++)
+                        {
+                            Header = Header + " ";
+                        }
+
+                        bHeader = Header.getBytes();
+                        
+                        //Write header into pack file
+                        foobj.write(bHeader,0,100);
+
+                        // Read the data from input files from Marvellous folder
+                        while((iRet = fiobj.read(Buffer)) != -1)
+                        {
+                            // Encryption Logic
+                            for(j = 0; j < iRet; j++)
+                            {
+                                Buffer[j] = (byte)(Buffer[j] ^ Key);
+                            }
+
+                            //Write the files data into Pack file
+                            foobj.write(Buffer,0,iRet);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                System.out.println("There is no such folder");
+            }
+        }
+        catch(IOException eobj)
+        {
+            System.out.println(eobj);
+        }
+    }
+}
+
+
+public class Packer
+{
+    public static void main(String A[])
+    {
+        PackerX pobj = new PackerX();
+
+        pobj.packing();
+
+    }
+}
---
+++ PackerUnpacker/Unpacker.java
@@ -0,0 +1,143 @@
+import java.io.*;
+import java.util.*;
+
+class UnpackerX
+{
+        public int FileSize;
+        public int i;
+        public int iRet;
+
+        public byte Key;
+
+        public Scanner sobj;
+
+        public String FileName;
+        public String Header;
+        public String Tokens[];
+
+        public File fpackobj;
+        public File fobj;
+    
+        public FileInputStream fiobj;
+        public FileOutputStream foobj;
+    
+        public byte bHeader[];
+        public byte Buffer[];
+
+
+    public UnpackerX()
+    {
+        this.FileSize = 0;
+        this.i = 0;
+        this.iRet = 0;
+
+        this.Key = 0x11;
+
+        this.sobj = null;
+        this.FileName = null;
+        this.Header = null;
+        this.Tokens = null;
+
+        this.fpackobj = null;
+        this.fobj = null;
+    
+        this.fiobj = null;
+        this.foobj = null;
+    
+        this.bHeader = new byte[100];
+        this.Buffer = null;
+    }
+
+    protected void Finalized()
+    {
+        try
+        {
+            fiobj.close();
+            foobj.close();
+        }
+        catch(IOException aobj)
+        {
+            System.out.println(aobj);
+        }
+        fpackobj = null;
+        fobj = null;
+        sobj.close();
+    }
+
+    public void Unpacking()
+    {
+        sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name of packed file");
+        FileName = sobj.nextLine();
+
+        fpackobj = new File(FileName);
+
+        if(fpackobj.exists() == false)
+        {
+            System.out.println("Error : There is no such packer file");
+            return;
+        }
+        
+        try
+        {
+            fiobj = new FileInputStream(fpackobj);
+
+            // Read the Header
+            while((iRet = fiobj.read(bHeader,0,100)) != -1)
+            {
+                Header = new String(bHeader);
+
+                Header = Header.trim();
+
+                Tokens = Header.split(" ");
+
+                System.out.println("File name : "+Tokens[0]);
+                System.out.println("File size : "+Tokens[1]);
+
+                fobj = new File(Tokens[0]);
+
+                fobj.createNewFile();
+
+                foobj = new FileOutputStream((fobj));
+
+                FileSize = Integer.parseInt(Tokens[1]);
+
+                // Buffer for reading the data
+                Buffer = new byte[FileSize];
+
+                // Read from packed file
+                fiobj.read(Buffer,0,FileSize);
+
+                // Decrypte the data
+                for(i = 0; i < FileSize; i++)
+                {
+                    Buffer[i] = (byte)(Buffer[i] ^ Key);
+                }
+                // Write into extracted file
+                foobj.write(Buffer,0,FileSize);
+            }
+        }
+        catch(IOException aobj)
+        {
+            System.out.println(aobj);
+        }
+        catch(NumberFormatException eobj)
+        {
+            System.out.println(eobj);
+        }
+    }
+}
+
+
+class Unpacker
+{
+    public static void main(String A[])
+    {
+
+        UnpackerX uobj = new UnpackerX();
+
+        uobj.Unpacking();
+
+    }
+}
