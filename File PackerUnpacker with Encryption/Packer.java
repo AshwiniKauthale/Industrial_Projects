1f789da5e390f1a8ddb8b6fc21b9cb86f2fc4b02
AshwiniKauthale <kauthaleashwini40@gmail.com>
2026-03-03 00:03:13 +0530

    Replace Logobj.close() with logWriter.close()

--- File PackerUnpacker with Encryption/Packer.java
+++ File PackerUnpacker with Encryption/Packer.java
@@ -84,7 +84,7 @@ class PackerX
         {
             fiobj.close();
             foobj.close();
-            Logobj.close();
+            logWriter.close();
         }
         catch(IOException aobj)
         {
